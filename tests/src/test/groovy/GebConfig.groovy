5a2990d7248c8ebdcace3067390e2c7c70b7a0d2
Fabian Trampusch <fabian.trampusch@isys.de>
2018-02-15 11:41:46 +0100

    update test libraries and fix chrome on travis

--- tests/src/test/groovy/GebConfig.groovy
+++ tests/src/test/groovy/GebConfig.groovy
@@ -1,3 +1,17 @@
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.remote.DesiredCapabilities
+
 waiting {
     timeout = 10
+}
+
+driver = {
+    ChromeOptions options = new ChromeOptions()
+    options.addArguments("--headless", "--no-sandbox", "--disable-gpu")
+
+    DesiredCapabilities capabilities = DesiredCapabilities.chrome()
+    capabilities.setCapability(ChromeOptions.CAPABILITY, options)
+
+    new ChromeDriver(capabilities)
 }
